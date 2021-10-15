0eb24c850d14e376b4e7fcf7e0d4aad1c30a0a6c
JSARAVIA017 <84134979+JSARAVIA017@users.noreply.github.com>
2021-10-15 17:46:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let sexo = prompt("cual es tu Sexo? (complete M o F)");
+
+if (sexo == "M") {
+    alert("Usted es Mascuino");
+} else if (sexo == "F") {
+    alert("Usted es Femenino");
+} else {
+    alert("Usted ingreso una letra incorrecta o es no binario");
+} 
